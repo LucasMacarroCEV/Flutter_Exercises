bedfadb35eee55a252ddffd2d93ea5e365127208
Lucas <lucas_macarro_m-tsapp1ma2324@cev.com>
2025-02-12 10:36:54 +0100

    Flutter_Exercises - Exercise 6 2/2

--- flutter_exercises/lib/Exercises/exercise_6.dart
+++ flutter_exercises/lib/Exercises/exercise_6.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:random_datetime/random_datetime.dart';
@@ -12,6 +13,10 @@ class MeetingRoom {
   MeetingRoom(this.name, this.date, this.people);
 }
 
+List<MeetingRoom> meetingRoomsList = [];
+List<MeetingRoom> reservedMeetingRoomsList = [];
+
+//------>  NavBar
 class Exercise6 extends StatefulWidget {
   const Exercise6({super.key});
 
@@ -20,8 +25,55 @@ class Exercise6 extends StatefulWidget {
 }
 
 class _Exercise6ViewState extends State<Exercise6> {
-  List<MeetingRoom> meetingRoomsList = [];
-  List<MeetingRoom> reservedMeetingRoomsList = [];
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    setMeetingRooms();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  static final List<Widget> _widgetOptions = [
+    const Exercise6Page(),
+    const Exercise6Reserved()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _widgetOptions.elementAt(_currentIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromRGBO(180, 180, 180, 1.0),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: Color.fromRGBO(10, 10, 10, 1),
+            ),
+            label: "Salas de reuniones",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star, color: Color.fromRGBO(240, 180, 55, 1)),
+            label: "Reservas",
+          )
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Color.fromRGBO(255, 255, 255, 1.0),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
 
   void setMeetingRooms() {
     int roomNum = getRandomNumber(1, 12);
@@ -29,13 +81,58 @@ class _Exercise6ViewState extends State<Exercise6> {
       meetingRoomsList.add(
           MeetingRoom(getRandomName(), getRandomDate(), getRandomPeople()));
     } while (meetingRoomsList.length < roomNum);
-    //setState(() {});
   }
 
+  int getRandomNumber(int min, int max) {
+    return min + Random().nextInt(max - min);
+  }
+
+  String getRandomName() {
+    List<int> nums = [];
+    int num;
+    do {
+      num = getRandomNumber(1, 20);
+    } while (nums.contains(num));
+    nums.add(num);
+    return "Sala $num";
+  }
+
+  int getRandomPeople() {
+    List<int> nums = [];
+    int num;
+    do {
+      num = getRandomNumber(1, 20);
+    } while (nums.contains(num));
+    nums.add(num);
+    return num;
+  }
+
+  String getRandomDate() {
+    DateTime currentDate = DateTime.now();
+    final dateOptions = RandomDTOptions(
+      allowPastDates: true,
+      futureYearLimit: 0,
+      startYear: currentDate.year - 1,
+    );
+    final randomDT = RandomDateTime(options: dateOptions);
+    final randomDateTime = randomDT.random();
+    var formatter = DateFormat("dd-MM-yyyy");
+    return formatter.format(randomDateTime);
+  }
+}
+
+//------>  Página principal que muestra las salas de reuniones
+class Exercise6Page extends StatefulWidget {
+  const Exercise6Page({super.key});
+
+  @override
+  State<Exercise6Page> createState() => _Exercise6PageState();
+}
+
+class _Exercise6PageState extends State<Exercise6Page> {
   @override
   void initState() {
     super.initState();
-    setMeetingRooms();
   }
 
   @override
@@ -96,15 +193,67 @@ class _Exercise6ViewState extends State<Exercise6> {
                               child: Column(
                                 spacing: 15,
                                 children: [
+                                  Text("Salas de reuniones",
+                                      style: TextStyle(
+                                          fontSize: 30, color: Colors.black87)),
                                   ListView.builder(
                                       shrinkWrap: true,
                                       itemCount: meetingRoomsList.length,
                                       itemBuilder: (context, index) {
-                                        return Text(
-                                            "${meetingRoomsList[index].name} ${meetingRoomsList[index].date} ${meetingRoomsList[index].people}",
-                                            style: TextStyle(
-                                                fontSize: 15,
-                                                color: Colors.black87));
+                                        return Container(
+                                            decoration: BoxDecoration(
+                                                border: Border.all(
+                                                    width: 1,
+                                                    color: Colors.black87),
+                                                borderRadius:
+                                                    const BorderRadius.all(
+                                                        Radius.circular(10)),
+                                                boxShadow: [
+                                                  BoxShadow(
+                                                      color: Colors.grey
+                                                          .withOpacity(0.5),
+                                                      spreadRadius: 2.5,
+                                                      blurRadius: 3.5,
+                                                      offset:
+                                                          const Offset(2, 4))
+                                                ]),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              spacing: 25,
+                                              children: [
+                                                IconButton(
+                                                  onPressed: () =>
+                                                      _onReserveButtonPressed(
+                                                          index),
+                                                  tooltip: "Reserve",
+                                                  icon: const Icon(Icons
+                                                      .bookmark_add_outlined),
+                                                ),
+                                                Text(
+                                                    meetingRoomsList[index]
+                                                        .name,
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                        color: Colors.black87)),
+                                                Text(
+                                                    "${meetingRoomsList[index].people} personas",
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        color: Colors.black87)),
+                                                Text(
+                                                    textAlign: TextAlign.end,
+                                                    meetingRoomsList[index]
+                                                        .date,
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontStyle:
+                                                            FontStyle.italic,
+                                                        color: Colors.black87))
+                                              ],
+                                            ));
                                       })
                                 ],
                               ),
@@ -119,40 +268,166 @@ class _Exercise6ViewState extends State<Exercise6> {
     Navigator.pop(context);
   }
 
-  int getRandomNumber(int min, int max) {
-    return min + Random().nextInt(max - min);
+  void _onReserveButtonPressed(int index) {
+    reservedMeetingRoomsList.add(meetingRoomsList[index]);
+    meetingRoomsList.removeAt(index);
+    setState(() {});
+  }
+}
+
+//------>  Página que muestra las salas de reuniones reservadas
+class Exercise6Reserved extends StatefulWidget {
+  const Exercise6Reserved({super.key});
+
+  @override
+  State<Exercise6Reserved> createState() => _Exercise6ReservedState();
+}
+
+class _Exercise6ReservedState extends State<Exercise6Reserved> {
+  @override
+  void initState() {
+    super.initState();
   }
 
-  String getRandomName() {
-    List<int> nums = [];
-    int num;
-    do {
-      num = getRandomNumber(1, 20);
-    } while (nums.contains(num));
-    nums.add(num);
-    return "Sala $num";
+  @override
+  void dispose() {
+    super.dispose();
   }
 
-  int getRandomPeople() {
-    List<int> nums = [];
-    int num;
-    do {
-      num = getRandomNumber(1, 20);
-    } while (nums.contains(num));
-    nums.add(num);
-    return num;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  spacing: 20,
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: _onExitButtonPressed,
+                          icon: const Icon(Icons.arrow_back_sharp),
+                          iconSize: 35,
+                          color: Colors.black87,
+                        )
+                      ],
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Ejercicio 6",
+                            style:
+                                TextStyle(fontSize: 40, color: Colors.black87))
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                            width: 450,
+                            decoration: BoxDecoration(
+                              color:
+                                  Theme.of(context).colorScheme.inversePrimary,
+                              border:
+                                  Border.all(width: 2, color: Colors.black87),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: const Offset(0, 3),
+                                )
+                              ],
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(20),
+                              child: Column(
+                                spacing: 15,
+                                children: [
+                                  Text("Salas reservadas",
+                                      style: TextStyle(
+                                          fontSize: 30, color: Colors.black87)),
+                                  ListView.builder(
+                                      shrinkWrap: true,
+                                      itemCount:
+                                          reservedMeetingRoomsList.length,
+                                      itemBuilder: (context, index) {
+                                        return Container(
+                                            decoration: BoxDecoration(
+                                                border: Border.all(
+                                                    width: 1,
+                                                    color: Colors.black87),
+                                                borderRadius:
+                                                    const BorderRadius.all(
+                                                        Radius.circular(10)),
+                                                boxShadow: [
+                                                  BoxShadow(
+                                                      color: Colors.grey
+                                                          .withOpacity(0.5),
+                                                      spreadRadius: 2.5,
+                                                      blurRadius: 3.5,
+                                                      offset:
+                                                          const Offset(2, 4))
+                                                ]),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              spacing: 25,
+                                              children: [
+                                                IconButton(
+                                                  onPressed: () =>
+                                                      _onCancelReservationButtonPressed(
+                                                          index),
+                                                  tooltip: "Cancel reservation",
+                                                  icon: const Icon(
+                                                      Icons.bookmark),
+                                                ),
+                                                Text(
+                                                    reservedMeetingRoomsList[
+                                                            index]
+                                                        .name,
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                        color: Colors.black87)),
+                                                Text(
+                                                    "${reservedMeetingRoomsList[index].people} personas",
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        color: Colors.black87)),
+                                                Text(
+                                                    textAlign: TextAlign.end,
+                                                    reservedMeetingRoomsList[
+                                                            index]
+                                                        .date,
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontStyle:
+                                                            FontStyle.italic,
+                                                        color: Colors.black87))
+                                              ],
+                                            ));
+                                      })
+                                ],
+                              ),
+                            ))
+                      ],
+                    )
+                  ],
+                ))));
   }
 
-  String getRandomDate() {
-    DateTime currentDate = DateTime.now();
-    final dateOptions = RandomDTOptions(
-      allowPastDates: true,
-      futureYearLimit: 0,
-      startYear: currentDate.year - 1,
-    );
-    final randomDT = RandomDateTime(options: dateOptions);
-    final randomDateTime = randomDT.random();
-    var formatter = DateFormat("dd-MM-yyyy");
-    return formatter.format(randomDateTime);
+  void _onExitButtonPressed() {
+    Navigator.pop(context);
+  }
+
+  void _onCancelReservationButtonPressed(int index) {
+    meetingRoomsList.add(reservedMeetingRoomsList[index]);
+    reservedMeetingRoomsList.removeAt(index);
+    setState(() {});
   }
 }
